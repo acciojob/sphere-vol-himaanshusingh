009033778dfa844a90b341e4b34c2b5b26ec1397
Himanshu Singh <himanshusingh1k@gmail.com>
2025-11-12 16:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ function volume_sphere() {
   event.preventDefault();
   const radiusInput = document.getElementById("radius");
   const volumeOutput = document.getElementById("volume");
-  const radius = parseFloat(radiusInput.value);
+  const radius = Number(radiusInput.value);
   if (isNaN(radius) || radius < 0) volumeOutput.value = "NaN";
-  else {
-    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-    volumeOutput.value = volume.toFixed(4);
-  }
+  else volumeOutput.value = ((4 / 3) * Math.PI * radius ** 3).toFixed(4);
 }
-window.onload = document.getElementById("MyForm").onsubmit = volume_sphere;
+const form = document.getElementById("MyForm");
+form.addEventListener("submit", () => volume_sphere());
